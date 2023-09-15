99d2a0cb08de11d0c67e90c08afe13b9c75910b5
Amadi Daniel <danielamadi000@gmail.com>
2023-09-15 17:35:18 +0100

    add files

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# stage1
+# **Stage 2 - A simple CRUD Api written in NODE.JS.**
+
+## **Description**
+
+This API is written using Node.js, with Express as the web framework, and MongoDB as the database. The API allows users to create, retrieve, update, and delete a products data from the database. The project's entry point is `server.js`.
+
+## **API Documentation**
+
+The API is fully documented, and can be found [here](https://documenter.getpostman.com/view/29761325/2s9YC5zYqn).
+
+## UML Diagram
+
+![UML Diagram](image.png)
+
+## Testing
+
+Unit tests/feature tests were not part of the requirements, but from the API docs in the link above, you will be able to test it on Postman.
+The API is deployed to [robinsonuche.onrender.com](https://robinsonuche.onrender.com/api)
+
+## **Known Limitations and Assumptions**
+
+This API assumes that product records are uniquely identified by their _id provided by MongoDB. The API uses MongoDB for storage. Error responses are standardized but can be further customized to meet specific requirements.
+
+## **Local Setup:**
+
+* Install the latest version of Node.js.
+* Clone this repository.
+  ```git clone https://github.com/nielsen2e/HNGx.git```
+* Change Branch
+  ```git checkout stage2```
+* Install the required packages:
+  ```npm install express mongoose body-parser```
+* Ensure you have MongoDB installed and running.
+* Run the application:
+  ``` node index.js ```
+* The API will be accessible locally at <http://127.0.0.1:5000/api>
+
--- server.js
+++ server.js
@@ -1,13 +1,87 @@
+require('dotenv').config();
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
+app.use(express.json())
 
 //routes
 
-app.get('/', (req, res) => {
-    res.send('Hello Node Api')
+// Fetch all products
+app.get('/api', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }   
 })
 
-app.listen(3000, ()=> {
-    console.log('Node Api app is running on port 3000')
+// Fetch product by id
+app.get('/api/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})  
+    }
 })
+
+// Add a product
+app.post('/api', async(req, res) => {
+    try {
+       const product = await Product.create(req.body)
+       res.status(200).json(product);
+
+    } catch (error) {
+       console.log(error.message);
+       res.status(500).json({message: error.message}) 
+    }
+})
+
+//update a product
+app.put('/api/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//delete a product
+app.delete('/api/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+        app.listen(3000, ()=> {
+            console.log('Node Api app is running on port 3000')
+        })
+    })
+    .catch(() => {
+        console.log('Error connecting to MongoDB');
+    });
